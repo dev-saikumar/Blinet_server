d7f984a13f0637f632591b0132de99f027ca741a
dev-saikumar <saikumarreddyatlrui@gmail.com>
2022-10-11 23:51:01 +0530

    node and vercel commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const app=express();
-const bodyparser=require('bodyparser');
+const bodyparser=require('body-parser');
 const loginpage=require('./routes/login');
  
 
 
 
 app.use(bodyparser.json());
-
+app.get("/",(rq,res)=>{
+res.status(200).send("u have invoked me");
+});
 app.use("/login",loginpage);
 
 app.listen("3000",()=>{
--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,23 @@
 const express=require('express');
-
 const route=express.Router();
+const Pool=require('pg').Pool;
+const creds=require('../db');
+
+const credentials= new Pool(creds);
 
 route.get("/",(req,res)=>{
     var e_mail=req.query.email;
-    var pass=req.query.password;
-    var userJson=`{"user_id":1,"email":${e_mail},"name":saikumar}`;
-    if(email=="22m0745@iitb.ac.in"&&password=="audibenz"){
+    var pass=req.query.pass;
+    console.log(req.query.email);
+    var name="saikumar"
+    var userJson=`{"user_id":1,"email":${e_mail.toString()},"name":${name.toString()}}`;
+    if(e_mail=="22m0745@iitb.ac.in"&&pass=="audibenz"){
         res.statusCode=200;
         res.send(userJson);
     }else{
-        res.statusCode=404;
-        res.send("wrong credentials");
-    }
+    res.status(200).send("u have invoked entered wrong credentials");
+}
 });
 
 
-module.exports==route;
+module.exports=route;
