26ffdd4c910ee2dcc13d808468298d733412c405
dev-saikumar <saikumarreddyatlrui@gmail.com>
2022-10-12 00:27:26 +0530

    initial pages commit

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ const loginpage=require('./routes/login');
 
 app.use(bodyparser.json());
 app.get("/",(rq,res)=>{
-res.status(200).send("u have invoked me");
+res.status(200).sendFile(__dirname+"/html_files/login.html");
 });
+
+app.get("/register",(rq,res)=>{
+    res.status(200).sendFile(__dirname+"/html_files/register.html");
+    });
+
+app.get("/homepage",(rq,res)=>{
+    res.status(200).sendFile(__dirname+"/html_files/home.html");
+    });
+
 app.use("/login",loginpage);
 
 app.listen("3000",()=>{
